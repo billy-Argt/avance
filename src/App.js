9845403947402047f505c8164293074dd76610ed
Billy Alegria <billy.argt@gmail.com>
2023-09-06 10:14:44 -0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import CreditCardForm from './CreditCardForm';
+
+
+function App() {
+  return (
+    <div className="App">
+      <CreditCardForm />
+    </div>
+  );
+}
+
+export default App;
+
+
